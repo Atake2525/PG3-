ebfe386c7600f64132222ffcfc52ed3073d4f624
Atake <146906117+Atake2525@users.noreply.github.com>
2024-10-22 10:07:28 +0900

    確認課題遅れ

--- main.cpp
+++ main.cpp
@@ -1,19 +1,52 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+#include <stdlib.h>
 
-int Recursive(int n, int result) {
-	if (n <= 0) {
-		return result;
+
+
+typedef void (*PFunc)(int*);
+
+// コールバック関数
+void DispResult(int* result) {
+	if (*result % 2 == 0) {
+		printf("グウスウ\n", *result);
+	} else {
+		printf("キスウ\n", *result);
 	}
-
-	return Recursive(n - 1, result * 2 - 50);
-	return 0;
 }
 
-int main() { 
-	int result = 100;
-	int n = 5;
-	result = Recursive(n, result);
-	printf("%d", result);
+void setTimeout(PFunc p, int second) {
+	// コールバック関数を呼び出す
+	Sleep(second * 1000);
+	p(&second);
+}
+
+int main() {
+	int dice;
+	int s;
+
+	printf("サイコロをフります\n");
+
+	// サイコロを振る（1〜6のランダムな数値を生成）
+	srand((unsigned int)time(NULL));
+	dice = rand() % 6 + 1;
+	dice = dice % 2;
+
+	// ユーザーに1か2で入力させる
+	printf("サイコロのデメがハンかチョウかをニュウリョクしてください\n");
+	printf("1がハン, 2がチョウ: ");
+	scanf_s("%d", &s);
+
+	// 3秒後に結果を表示
+	setTimeout(DispResult, 3);
+
+	// 回答を判定
+	if ((dice == 1 && s == 2) || (dice == 2 && s == 1)) {
+		printf("セイカイ\n");
+	} else {
+		printf("フセイカイ\n");
+	}
 
 	return 0;
 }
